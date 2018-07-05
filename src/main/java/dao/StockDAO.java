4a6138e150726302deda90dd620ebff5fb66038d
Sara Chamseddine <sarychamsf@hotmail.com>
2018-07-05 15:18:58 -0500

    Comillas

--- src/main/java/dao/StockDAO.java
+++ src/main/java/dao/StockDAO.java
@@ -54,7 +54,7 @@ public class StockDAO {
     }
 
     public void updateStock(String nombre, Stock stock) throws SQLException {
-        PreparedStatement preparedStatement = connection.prepareStatement("update stock set cantidad=?" + " where nombre=?");
+        PreparedStatement preparedStatement = connection.prepareStatement("update stock set cantidad=?" + " where nombre=\'?"+"\'");
         preparedStatement.setFloat(1, stock.getCantidad());
         preparedStatement.setString(2, nombre);
         preparedStatement.executeUpdate();
